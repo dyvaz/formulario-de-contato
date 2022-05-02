8767e67cdaedeca38e9d0fb5873bd3cb1bedcfd3
Dyanna Azevedo <dyvazevedo@gmail.com>
2022-05-02 18:52:34 -0300

    atualizaçao do sql

--- php-mysql/Script.sql
+++ php-mysql/Script.sql
@@ -4,5 +4,5 @@ name VARCHAR(45) NOT NULL,
 email VARCHAR(60) NOT NULL,
 message TEXT NOT NULL,
 visualized BOOL NOT NULL,
-moment CURRENT_TIMESTAMP DEFAULT_GENERATED,
+moment datetime NOT NULL default NOW(),
 PRIMARY KEY (id))
