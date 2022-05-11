5bfb3d8a0a1dc3ee51b9895f2ff3086eb84f8a1d
Dyanna Azevedo <dyvazevedo@gmail.com>
2022-05-11 14:10:55 -0300

    formatação do script.sql

--- script.sql
+++ script.sql
@@ -1,8 +1,9 @@
 CREATE table contact_form (
-id INT NOT NULL auto_increment,
-name VARCHAR(45) NOT NULL,
-email VARCHAR(60) NOT NULL,
-message TEXT NOT NULL,
-visualized BOOL NOT NULL,
-moment datetime NOT NULL default NOW(),
-PRIMARY KEY (id))
+    id INT NOT NULL auto_increment,
+    name VARCHAR(45) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    message TEXT NOT NULL,
+    visualized BOOL NOT NULL,
+    moment datetime NOT NULL default NOW(),
+    PRIMARY KEY (id)
+)
